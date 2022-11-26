474312e6d58db7bcbae5a81114117957bcbad034
Yu Ho Hyeon <117062358+geloyu@users.noreply.github.com>
2022-11-26 14:57:47 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.14 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, Integer> idIndex = new HashMap<>();
+        Map<String, List<String>> reportMap = new HashMap<>();
+
+        for (int i = 0; i < id_list.length; i++) {
+            idIndex.put(id_list[i], i);
+            reportMap.put(id_list[i], new ArrayList<>());
+        }
+
+        for (String reported : report) {
+            String[] temp = reported.split(" ");
+            if (!reportMap.get(temp[1]).contains(temp[0])) {
+                reportMap.get(temp[1]).add(temp[0]);
+            }
+        }
+
+        for (String id : reportMap.keySet()) {
+            if (k <= reportMap.get(id).size()) {
+                for (String reporter : reportMap.get(id)) {
+                    answer[idIndex.get(reporter)]++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
